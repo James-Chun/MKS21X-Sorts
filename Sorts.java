5c90e2f0a787d04026eadcfe347ceb1620a8b56f
James-Chun <jchun00@stuy.edu>
2018-12-04 23:50:00 -0500

    nvm it doesn't work so I changed up my method to use a bunch of for loops

--- Sorts.java
+++ Sorts.java
@@ -1,22 +1,12 @@
 public class Sorts{
-  public static void selectionsort(int[] ary){
-    int[] newArray = ary;
-    int compareValue = ary[0];  //This is the temp value that will be compared to all the other values in the list and if it is the smallest will be added to the "front".
-    int tempPlace = 0;  //border between ints already sorted and ints to be sorted (also the place where smallest int will be placed)
-    for (int i=tempPlace;tempPlace<ary.length;i++){
-      if (compareValue>ary[i]) compareValue=ary[i];
-      if (i==ary.length-1) {
-        ary[tempPlace]=compareValue;
-        tempPlace++; //once i has reached end of list and tempPlace has not, reset i back to tempPlace;
-        i=tempPlace;
+  public static boolean contains(int[] ary,int n){
+    int indexOfSmallest = 0; //index of the smallest value
+    int smallest = ary[0]; //smallest index 
+    for (int i=0; i<ary.length;i++){
+      smallest = ary[i];
+      for (int i2=0;i<ary.length;i++){
+
       }
-    }
-  }
-  public static void main(String[] args){
-    int[] n = {1,2,4,62,2,4};
-    selectionsort(n);
-    for (int i=0;i<n.length;i++){
-      System.out.println(n[i]);
     }
   }
 }
