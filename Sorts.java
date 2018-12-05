11dcb31a8ada767b49bbc4b9e0c24d8d86e2ae8a
James-Chun <jchun00@stuy.edu>
2018-12-04 21:25:40 -0500

    started

---
+++ Sorts.java
@@ -0,0 +1,13 @@
+public class Sorts{
+  public static void selectionsort(int[] ary){
+    int compareValue = ary[1];
+    int tempPlace = 0;
+    for (int i=tempPlace+1;place<ary.length;i++){
+      if (i==ary.length)i=tempPlace+1;
+      if (compareValue>ary[i]){
+        temp=ary[i];
+        place=
+      }
+    }
+  }
+}
