f6a6828a6b27429cc81112b1e08783ad4a001b00
James Chun <jchun00@stuy.edu>
2018-12-11 10:48:03 -0500

    made bubble better and made method to check if data is already sorted

--- Sorts.java
+++ Sorts.java
@@ -1,30 +1,45 @@
 import java.util.Random;
 
 public class Sorts{
+
+  private static boolean sorted(int[] data){
+    for (int i =1;i<data.length;i++){
+      if (data[i]<data[i-1]){
+        return false;
+      }
+    }
+    return true;
+  }
+
   public static void selectionSort(int[] ary){
-    int indexOfSmallest = 0; //index of the smallest value
-    int smallest = ary[0]; //smallest index
-    for (int i=0; i<ary.length;i++){
-      smallest = ary[i];
-      for (int i2=i;i2<ary.length;i2++){ //starting from last position, see if all the ones after it are smaller
-        if (smallest>ary[i2])smallest=ary[i2]; //adjusting smallest
+    if (!sorted(ary)){
+      int indexOfSmallest = 0; //index of the smallest value
+      int smallest = ary[0]; //smallest index
+      for (int i=0; i<ary.length;i++){
+        smallest = ary[i];
+        for (int i2=i;i2<ary.length;i2++){ //starting from last position, see if all the ones after it are smaller
+          if (smallest>ary[i2])smallest=ary[i2]; //adjusting smallest
+        }
+        for(int i3=0;i3<ary.length;i3++){
+          if (ary[i3]==smallest)indexOfSmallest=i3; //finding index of smallest value and storing it
+        }
+        ary[indexOfSmallest]=ary[i]; //switching smallest and value at i (which should increase sequentially)
+        ary[i]=smallest;
       }
-      for(int i3=0;i3<ary.length;i3++){
-        if (ary[i3]==smallest)indexOfSmallest=i3; //finding index of smallest value and storing it
-      }
-      ary[indexOfSmallest]=ary[i]; //switching smallest and value at i (which should increase sequentially)
-      ary[i]=smallest;
     }
   }
 
     public static void bubbleSort(int[] data){
-      for (int a =0;a<data.length;a++){
-        for (int i=1;i<data.length;i++){
-          int bigger = data[i]; //variable needed to store bigger value during swap
-          if (data[i]<data[i-1]){ //check to see if previous value is bigger than current
-            bigger = data[i-1]; // if bigger, then swap using temporary bigger variable
-            data[i-1]=data[i];
-            data[i]=bigger;
+      if (!(sorted(data))){
+        int bigger = data[0]; //variable needed to store bigger value during swap
+        for (int a =0;a<data.length;a++){
+          for (int i=1;i<data.length;i++){
+            bigger = data[i];
+            if (data[i]<data[i-1]){ //check to see if previous value is bigger than current
+              bigger = data[i-1]; // if bigger, then swap using temporary bigger variable
+              data[i-1]=data[i];
+              data[i]=bigger;
+            }
           }
         }
       }
@@ -50,20 +65,38 @@ public class Sorts{
     }
 
     public static void main(String[] args){
-      Random randgen = new Random();
-      int[] list = new int[Integer.parseInt(args[0])];
-      for (int i=0;i<Integer.parseInt(args[0]);i++){
-        list[i]=randgen.nextInt()/5555567;
+      if (args.length>0){
+        Random randgen = new Random();
+        int[] list = new int[Integer.parseInt(args[0])];
+        for (int i=0;i<Integer.parseInt(args[0]);i++){
+          list[i]=randgen.nextInt()/5555567;
+        }
+
+        selectionSort(list);
+
+        for (int i=0;i<Integer.parseInt(args[0]);i++){
+          System.out.println(list[i]);
+        }
+
+        //System.out.println(sorted(list));
+
+      }else{
+        int[] list = new int[1000000];
+        for (int i=0;i<1000000;i++){
+          list[i]=0;
+        }
+        bubbleSort(list);
+        selectionSort(list);
+
+        for (int i =0;i<10;i++){
+          System.out.println(list[i]);
+        }
+
       }
 
-      selectionSort(list);
-      System.out.println(list);
-
-      for (int i=0;i<Integer.parseInt(args[0]);i++){
-        System.out.println(list[i]);
-      }
 
     }
+
 
 
 
